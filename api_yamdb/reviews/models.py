9dbd51bfb2ed8cdd044a5f375a1cf5a1f9e7a53f
Mikhail Patrakov <daddy.mihail@yandex.ru>
2022-03-15 01:19:51 +0300

    '1'

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,2 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('USER', 'user'),
+        ('MODERATOR', 'moderator'),
+        ('ADMIN', 'admin'),
+    ]
+    email = models.EmailField(max_length=254, unique=True,
+                              blank=False, null=False)
+    first_name = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES,
+                            default='USER', verbose_name='role')
+    REQUIRED_FIELDS = ['username', 'email']
