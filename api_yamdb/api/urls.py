7e756bc5daa217fd0715cfcf2ba850b43696353f
Eugene Che <grem1in.che@yandex.ru>
2022-03-16 12:06:29 +0300

    Fic сategories

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 router = routers.SimpleRouter()
-router.register('сategories', CategoryViewSet)
+router.register('categories', CategoryViewSet)
 router.register('genres', GenreViewSet)
 router.register('titles', TitleViewSet)
 
