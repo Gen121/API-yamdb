08931e5526ceba39f6841422b18b7e4d23afe5d5
Mikhail Patrakov <daddy.mihail@yandex.ru>
2022-03-17 20:53:17 +0300

    'user3'

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -16,4 +16,7 @@ class AdminOrReadOnnly(permissions.BasePermission):
 class Admin(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.role == 'ADMIN'
+        try:
+            return 'ADMIN' == request.user.role
+        except AttributeError:
+            return False
